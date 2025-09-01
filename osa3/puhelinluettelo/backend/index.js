781b8c8be48840ae9a8b800387a1ea53bca145fa
Perttu Saarsalmi <ext.perttu.saarsalmi@metsagroup.com>
2025-09-01 13:36:31 +0300

    puhelinluettelo backend step6

--- osa3/puhelinluettelo/backend/index.js
+++ osa3/puhelinluettelo/backend/index.js
@@ -43,6 +43,18 @@ app.delete("/api/persons/:id", (request, response) => {
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
+  if (!body.number || !body.name) {
+    return response.status(400).json({ 
+      error: 'name or phone number is missing' 
+    })
+  }
+  
+  if (persons.map(person => person.name).includes(body.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+  
   const person = {
     number: body.number,
     name: body.name,
