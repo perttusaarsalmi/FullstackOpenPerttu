3126692216543f2b6d816c8a2adceae68873f325
Perttu Saarsalmi <ext.perttu.saarsalmi@metsagroup.com>
2025-09-01 13:42:36 +0300

    puhelinluettelo backend step7

--- osa3/puhelinluettelo/backend/index.js
+++ osa3/puhelinluettelo/backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 app.use(express.json());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 let persons = [
   { id: "1", name: "Arto Hellas", number: "040-123456" },
