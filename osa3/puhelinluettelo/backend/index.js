293d69a15e7e26c3ee6c4ab5a65a47f43d6d3320
Perttu Saarsalmi <ext.perttu.saarsalmi@metsagroup.com>
2025-09-01 13:07:48 +0300

    puhelinluettelo backend step3

--- osa3/puhelinluettelo/backend/index.js
+++ osa3/puhelinluettelo/backend/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 
 let persons = [
-  { name: "Arto Hellas", number: "040-123456" },
-  { name: "Ada Lovelace", number: "39-44-5323523" },
-  { name: "Dan Abramov", number: "12-43-234345" },
-  { name: "Mary Poppendieck", number: "39-23-6423122" },
+  { id: "1", name: "Arto Hellas", number: "040-123456" },
+  { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
+  { id: "3", name: "Dan Abramov", number: "12-43-234345" },
+  { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" },
 ];
 
 app.get("/info", (request, response) => {
@@ -21,6 +21,17 @@ app.get("/api/persons", (request, response) => {
   response.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
   response.end(JSON.stringify(persons));
 });
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
 
 const PORT = 3002;
 app.listen(PORT, () => {
