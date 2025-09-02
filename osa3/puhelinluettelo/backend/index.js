f158697b81d4f5c2662f75aecdcfd88192033481
Perttu Saarsalmi <ext.perttu.saarsalmi@metsagroup.com>
2025-09-02 16:08:15 +0300

    puhelinluettelo ja tietokanta step1

--- osa3/puhelinluettelo/backend/index.js
+++ osa3/puhelinluettelo/backend/index.js
@@ -2,38 +2,16 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
-const mongoose = require('mongoose')
-require('dotenv').config()
+const Person = require("./models/person");
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 
-//const password = process.argv[2]
-// const url = `mongodb+srv://perttusaarsalmi_db_user:${password}@cluster0.ovwckyn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-
-const url = process.env.MONGODB_URI
-console.log(url);
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
-
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
-
-const Person = mongoose.model('Person', personSchema)
-
-let persons = [
-  { id: "1", name: "Arto Hellas", number: "040-123456" },
-  { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
-  { id: "3", name: "Dan Abramov", number: "12-43-234345" },
-  { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" },
-];
+  console.log(`Server running on port ${PORT}`);
+});
 
 morgan.token("body", (req) => {
   return req.method === "POST" || req.method === "PUT"
@@ -43,34 +21,36 @@ morgan.token("body", (req) => {
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
-app.use(express.static('dist'));
-
+app.use(express.static("dist"));
 
 app.get("/info", (request, response) => {
   response.writeHead(200, {});
-  response.end(
-    `Phonebook has info for ${
-      persons.length
-    } people\n\n${new Date().toString()}`
-  );
+  Person.find({}).then((persons) => {
+    response.end(
+      `Phonebook has info for ${
+        persons.length
+      } people\n\n${new Date().toString()}`
+    );
+  });
 });
 
 app.get("/api/persons", (request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json; charset=utf-8" });
+  response.writeHead(200, {
+    "Content-Type": "application/json; charset=utf-8",
+  });
   Person.find({}).then((persons) => {
     response.end(JSON.stringify(persons));
   });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  Person.findById(request.params.id).then((person) => {
+    if (person) {
+      response.json(person);
+    } else {
+      response.status(404).end();
+    }
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
