929ffc15c57c089bbbb3099c19e0aafd24b6c5d3
Perttu Saarsalmi <ext.perttu.saarsalmi@metsagroup.com>
2025-09-03 12:26:29 +0300

    uibuild

--- osa3/puhelinluettelo/backend/models/person.js
+++ osa3/puhelinluettelo/backend/models/person.js
@@ -10,9 +10,18 @@ const personSchema = new mongoose.Schema({
   name: {
     type: String,
     minLength: 3,
-    required: true
+    required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+         return /^\d{2,3}-\d{5,}$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "User phone number required"],
+  },
 });
 
 personSchema.set("toJSON", {
