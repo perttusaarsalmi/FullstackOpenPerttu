200d9dd790e4347307319390e69fc0ae3084acc0
Perttu Saarsalmi <ext.perttu.saarsalmi@metsagroup.com>
2025-09-15 11:54:29 +0300

    Redux anecdotes steps 1 and 2

---
+++ osa6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,45 @@
+import { useSelector, useDispatch } from 'react-redux'
+
+const App = () => {
+  const anecdotes = useSelector((state) => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch({ type: 'VOTE', payload: id })
+  }
+
+  const addAnecdote = (content) => {
+    dispatch({ type: 'NEW_ANECDOTE', payload: content })
+  }
+
+  const handleCreateAnecdote = (e) => {
+    e.preventDefault()
+    const content = e.target.anecdote.value
+    addAnecdote(content)
+    e.target.anecdote.value = ''
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+      <h2>create new</h2>
+      <form onSubmit={handleCreateAnecdote}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
