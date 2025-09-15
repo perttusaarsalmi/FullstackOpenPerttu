46259a263b5a4d3fbb56c36743c6d6d176bb4955
Perttu Saarsalmi <ext.perttu.saarsalmi@metsagroup.com>
2025-09-15 14:27:53 +0300

    Redux anecdotes steps 8 ja 9

--- osa6/redux-anecdotes/src/main.jsx
+++ osa6/redux-anecdotes/src/main.jsx
@@ -1,16 +1,10 @@
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import store from './store'
 import App from './App'
-import anecdoteReducer from './reducers/anecdoteReducer'
-import filterReducer from './reducers/filterReducer'
 
-const rootReducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  filter: filterReducer,
-})
 
-const store = createStore(rootReducer)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
