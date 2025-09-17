dac7d74752c881a6a5c678204449d3e76811d705
Perttu Saarsalmi <ext.perttu.saarsalmi@metsagroup.com>
2025-09-17 16:23:40 +0300

    Custom hook anecdotes step1

--- osa7/routed-anecdotes/src/App.jsx
+++ osa7/routed-anecdotes/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useField } from './hooks'
 import { Route, Routes, Link, useParams, useNavigate } from 'react-router-dom'
 
 const Menu = () => {
@@ -79,21 +80,22 @@ const Notification = ({ message }) =>
   ) : null
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
   const navigate = useNavigate()
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     })
-    props.showNotification(content)
+    props.showNotification(content.value)
     navigate('/') // uudelleenohjaus juureen
   }
+
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   return (
     <div>
@@ -101,27 +103,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
