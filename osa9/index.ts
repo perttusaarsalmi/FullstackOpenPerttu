8ac8b4ccc1503e1f925146ac20117da34999aae7
Perttu Saarsalmi <ext.perttu.saarsalmi@metsagroup.com>
2025-09-22 16:22:19 +0300

    osa9 typescript step7

--- osa9/index.ts
+++ osa9/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { calculateBmi } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
 
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Full Stack!');
@@ -22,6 +24,37 @@ app.get('/bmi', (req, res) => {
     height,
     bmi,
   });
+});
+
+
+interface ExerciseRequest {
+  daily_exercises: number[];
+  target: number;
+}
+
+app.post('/exercises', (req, res) => {
+  const body: unknown = req.body;
+  if (!body || typeof body !== 'object') {
+    return res.status(400).json({ error: 'malformatted parameters' });
+  }
+  
+  const { daily_exercises, target } = body as ExerciseRequest;
+
+  if (!daily_exercises || target === undefined) {
+    return res.status(400).json({ error: 'parameters missing' });
+  }
+
+  if (!Array.isArray(daily_exercises) || isNaN(Number(target))) {
+    return res.status(400).json({ error: 'malformatted parameters' });
+  }
+
+  if (daily_exercises.some(exercise => isNaN(Number(exercise)))) {
+    return res.status(400).json({ error: 'malformatted parameters' });
+  }
+
+  const result = calculateExercises(daily_exercises, Number(target));
+  
+  return res.json(result);
 });
 
 const PORT = 3003;
