d61f87bd51ea86ce636b2609fd83b34d550ea573
Perttu Saarsalmi <ext.perttu.saarsalmi@metsagroup.com>
2025-09-23 09:07:55 +0300

    Patientor frontend cloned and backend fixed to work with frontend

--- osa9/patientor/backend/index.ts
+++ osa9/patientor/backend/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 const app = express();
 app.use(express.json());
+import cors from 'cors';
 
-const PORT = 3000;
+app.use(cors());
 
-app.get('/ping', (_req, res) => {
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
