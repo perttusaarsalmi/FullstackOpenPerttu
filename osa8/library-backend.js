32fcaa808604dbde45d0b4e4243a1c12e4322a46
Perttu Saarsalmi <ext.perttu.saarsalmi@metsagroup.com>
2025-10-02 09:35:05 +0300

    osa8 graphql step3

--- osa8/library-backend.js
+++ osa8/library-backend.js
@@ -106,10 +106,16 @@ const typeDefs = `
     genres: [String!]! 
   }
 
+  type Author {
+    name: String!
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `;
 
@@ -118,6 +124,11 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (parent) =>
+      books.filter((book) => book.author === parent.name).length,
   },
 };
 
