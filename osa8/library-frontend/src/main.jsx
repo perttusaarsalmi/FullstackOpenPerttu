1e6a68227fbda21d216170c9e251a8e32f22d121
Perttu Saarsalmi <ext.perttu.saarsalmi@metsagroup.com>
2025-10-02 12:45:12 +0300

    osa8 graphql step8

---
+++ osa8/library-frontend/src/main.jsx
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "http://localhost:4000" }), // 👈 use link instead of uri
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
