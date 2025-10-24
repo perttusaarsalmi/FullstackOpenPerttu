e7da7689bfefba3a4f94a96fb60878072f162ca3
Perttu Saarsalmi <ext.perttu.saarsalmi@metsagroup.com>
2025-10-24 13:20:34 +0300

    osa3 puhelinluettelo backend fixes

--- osa3/puhelinluettelo/backend/index.js
+++ osa3/puhelinluettelo/backend/index.js
@@ -13,7 +13,14 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
-app.use(morgan('tiny'))
+morgan.token('body', (req) => {
+  return req.method === 'POST' || req.method === 'PUT'
+    ? JSON.stringify(req.body)
+    : ''
+})
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 app.use(express.static('dist'))
 
 app.get('/info', (request, response, next) => {
