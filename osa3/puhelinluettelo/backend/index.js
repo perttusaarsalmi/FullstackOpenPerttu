184f075e22152910d7b01b93f6d362ffb1f94bf6
Perttu Saarsalmi <ext.perttu.saarsalmi@metsagroup.com>
2025-10-24 13:17:03 +0300

    morgan middleware related fixes

--- osa3/puhelinluettelo/backend/index.js
+++ osa3/puhelinluettelo/backend/index.js
@@ -13,14 +13,7 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
-morgan.token('body', (req) => {
-  return req.method === 'POST' || req.method === 'PUT'
-    ? JSON.stringify(req.body)
-    : ''
-})
-app.use(
-  morgan(':method :url :status :res[content-length] - :response-time ms :body')
-)
+app.use(morgan('tiny'))
 app.use(express.static('dist'))
 
 app.get('/info', (request, response, next) => {
